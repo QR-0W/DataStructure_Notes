d284f4878df7319903edd96bb1e224793050dacd
QR-0W <Neptune_X@foxmail.com>
2023-11-06 22:10:58 +0800

    Update to 6.0.0

--- func.cpp
+++ func.cpp
@@ -646,3 +646,26 @@ typedef struct CSNode
     ElemType data;
     struct CSNode *firstChild, *nextSibling;
 } CSNode, *CSTree;
+
+#define SIZE 100
+int UFSets[SIZE];
+
+void Initial(int S[])
+{
+    for (int i = 0; i < SIZE; i++)
+        S[i] = -1;
+}
+
+int Find(int S[], int x)
+{
+    while (S[x] >= 0)
+    {
+        x = S[x];
+    }
+    return x;
+}
+
+void Union(int S[], int Root1, int Root2)
+{
+    S[Root2] = Root1;
+}
